f88cdd3a75803e785c5e8acc42a19d8320aa7f3d
Hellen Bianchini <hellenbiangod@gmail.com>
2025-03-28 13:35:12 -0300

    (front-end) pagina iinicial view de clientes

---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="css\img\img1-sem-fundo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css\reset.css">
+
+    <link rel="stylesheet" href="css\index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Gravitas+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <title>Sua Cafeteria</title>
+</head>
+
+<body>
+    <main>
+        <header class="cabecalho">
+            <div class="container-img-logo">
+                <a href="">
+                    <img src="css\img\img1-sem-fundo.png" class="logo" alt="logo-cafe">
+                </a>
+            </div>
+            <h2>Sua Cafeteria</h2>
+            <a href="">Sair</a>
+        </header>
+        <div class="banner">
+            <img src="css\img\img2-sem-fundo.png" alt="">
+        </div>
+        <section class="container-cafe-manha">
+            <div class="container-cafe-manha-titulo">
+                <h3 id="titulo">~~Opções para o Café~~</h3>
+            </div>
+            <div class="container-cafe-manha-produtos">
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\cafe.jpg">
+                    </div>
+                    <p>Cafe Preto</p>
+                    <p>Cafe preto tradicional brasileiro</p>
+                    <p>R$ 3,00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\cafe-decor.jpg">
+                    </div>
+                    <p>Cafe Decorado</p>
+                    <p>Seu cafe com leite mais bonito</p>
+                    <p>R$ 7,00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\cafe-chocolate.webp">
+                    </div>
+                    <p>Cafe com Chocolate</p>
+                    <p>Para os fas de chocolate a melhor combinacao</p>
+                    <p>R$ 9,00</p>
+                </div>
+            </div>
+        </section>
+        <section class="container-almoco">
+            <div class="container-almoco-titulo">
+                <h3 id="titulo">~~Opções para o Almoco~~</h3>
+            </div>
+            <div class="container-almoco-produtos">
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\feijoada.jpeg">
+                    </div>
+                    <p>Feijoada</p>
+                    <p>O gosto brasileiro</p>
+                    <p>R$ 25,00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\file-de-frango-grelhado.jpeg">
+                    </div>
+                    <p>File de Frango</p>
+                    <p>File de Frango grelhado</p>
+                    <p>R$ 20,00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\peixe.webp">
+                    </div>
+                    <p>Peixe</p>
+                    <p>Peixe frito</p>
+                    <p>R$ 22,00</p>
+                </div>
+            </div>
+
+        </section>
+        <section class="container-bebidas">
+            <div class="container-bebidas-titulo">
+                <h3 id="titulo">~~Opções para Beber~~</h3>
+            </div>
+            <div class="container-bebidas-produtos">
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\suco.avif">
+                    </div>
+                    <p>Suco Laranja</p>
+                    <p>Suco no copo 500ml</p>
+                    <p>R$ 8,00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\coca.jpg">
+                    </div>
+                    <p>Coca-Cola</p>
+                    <p>Coca Lata</p>
+                    <p>R$ 6,00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="css\img\cerveja.jpg">
+                    </div>
+                    <p>Cerveja</p>
+                    <p>Heineken 600</p>
+                    <p>R$ 13,00</p>
+                </div>
+            </div>
+
+        </section>
+    </main>
+</body>
+
+</html>
