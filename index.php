c13247d6f67bc1bf9f2660cdd3fb2b12596c95a6
Hellen Bianchini <hellenbiangod@gmail.com>
2025-04-03 20:15:18 -0300

    feat - index

--- index.php
+++ index.php
@@ -9,49 +9,129 @@
 </head>
 
 <body>
-<main>
-    <header>
-        <div class="header-container">
-            <h1 id="titulo-header">Café Catalogo</h1>
-            <a id="link-header">Adicionar Produto</a>
-        </div>
-        <a class="button-login">Sair</a>
-    </header>
+    <main>
+        <header>
+            <div class="header-container">
+                <h1 id="titulo-header">Café Catalogo</h1>
+                <a id="link-header">Adicionar Produto</a>
+            </div>
+            <a class="button-login">Sair</a>
+        </header>
         <label>Lista de Produtos</label>
         <section>
-            <h4>Café</h4>
             <div class="container-cafe">
                 <div class="container-produto">
                     <div class="container-foto">
                         <img src="css\img\cafe-chocolate.webp">
                     </div>
-                    <p>Cafe com Chocolate</p>
-                    <p>Um cafe doce com sabor de chocolate</p>
-                    <p>R$ 5,00</p>
+                    <div class="texto-produto">
+                        <p class="titulo-produto">Cafe com Chocolate</p>
+                        <p class="descricao-produto">Um cafe doce com sabor de chocolate</p>
+                    </div>
+                    <div class="container-setins">
+                        <p class="preco-produto">R$ 5,00</p>
+                        <div class="setins"><a class="icon-edit"><i class="bi bi-pencil-square"><svg
+                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fill-rule="evenodd"
+                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                    </svg></i></a>
+                            <a class="icon-edit"><i class="bi bi-trash-fill"><svg xmlns="http://www.w3.org/2000/svg"
+                                        width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                                        viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                    </svg></i></a>
+                        </div>
+                    </div>
                 </div>
                 <div class="container-produto">
                     <div class="container-foto">
                         <img src="css\img\cafe-cremoso.jpg">
                     </div>
-                    <p>Cafe Cremoso</p>
-                    <p>Um cafe cremoso e saboroso</p>
-                    <p>R$ 7,00</p>
+                    <div class="texto-produto">
+                        <p class="titulo-produto">Cafe Cremoso</p>
+                        <p class="descricao-produto">Um cafe cremoso e saboroso</p>
+                    </div>
+
+                    <div class="container-setins">
+                        <p class="preco-produto">R$ 7,00</p>
+                        <div class="setins"><a class="icon-edit"><i class="bi bi-pencil-square"><svg
+                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fill-rule="evenodd"
+                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                    </svg></i></a>
+                            <a class="icon-edit"><i class="bi bi-trash-fill"><svg xmlns="http://www.w3.org/2000/svg"
+                                        width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                                        viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                    </svg></i></a>
+                        </div>
+                    </div>
                 </div>
                 <div class="container-produto">
                     <div class="container-foto">
                         <img src="css\img\cafe-decor.jpg">
                     </div>
-                    <p>Cafe Decorado</p>
-                    <p>Um cafe agradavel ao paladar e aos olhos</p>
-                    <p>R$ 10,00</p>
+                    <div class="texto-produto">
+
+                        <p class="titulo-produto">Cafe Decorado</p>
+                        <p class="descricao-produto">Um cafe agradavel ao paladar e aos olhos</p>
+                    </div>
+
+                    <div class="container-setins">
+                        <p class="preco-produto">R$ 9,00</p>
+                        <div class="setins"><a class="icon-edit"><i class="bi bi-pencil-square"><svg
+                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fill-rule="evenodd"
+                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                    </svg></i></a>
+                            <a class="icon-edit"><i class="bi bi-trash-fill"><svg xmlns="http://www.w3.org/2000/svg"
+                                        width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                                        viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                    </svg></i></a>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="container-produto">
                     <div class="container-foto">
                         <img src="css\img\cafe.jpg">
                     </div>
-                    <p>Cafe Preto</p>
-                    <p>Um cafe encorpado e de sabor intenso</p>
-                    <p>R$ 4,00</p>
+                    <div class="texto-produto">
+
+                        <p class="titulo-produto">Cafe Preto</p>
+                        <p class="descricao-produto">Um cafe encorpado e de sabor intenso</p>
+                    </div>
+                    <div class="container-setins">
+                        <p class="preco-produto">R$ 4,00</p>
+                        <div class="setins"><a class="icon-edit"><i class="bi bi-pencil-square"><svg
+                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fill-rule="evenodd"
+                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                    </svg></i></a>
+                            <a class="icon-edit"><i class="bi bi-trash-fill"><svg xmlns="http://www.w3.org/2000/svg"
+                                        width="16" height="16" fill="currentColor" class="bi bi-trash-fill"
+                                        viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                    </svg></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
