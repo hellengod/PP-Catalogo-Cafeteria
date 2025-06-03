07fb79f11e1ab5c0cd403b7ca8a950f0d7e6bec6
Hellen Bianchini <hellenbiangod@gmail.com>
2025-06-03 13:50:52 -0300

    adiciona location

--- novo-produto.php
+++ novo-produto.php
@@ -1,20 +1,24 @@
 <?php
 
-    $PDO = new PDO(
-        'mysql:host=' . '127.0.0.1' .
-        ';dbname=' . 'catcafe',
-        'root',
-        'root'
-    );
+$PDO = new PDO(
+    'mysql:host=' . '127.0.0.1' .
+    ';dbname=' . 'catcafe',
+    'root',
+    'root'
+);
 
-    $name = $_POST['name'];
-    $descricao = $_POST['descricao'];
-    $preco = $_POST['preco'];
+$name = $_POST['name'];
+$descricao = $_POST['descricao'];
+$preco = $_POST['preco'];
 
-    $sql = 'INSERT INTO produto (nome, Descricao, Preco) VALUES (?,?,?)';
-    $stmt = $PDO->prepare($sql);
-    $stmt->bindValue(1, $name);
-    $stmt->bindValue(2, $descricao);
-    $stmt->bindValue(3, $preco);
+$sql = 'INSERT INTO produto (nome, Descricao, Preco) VALUES (?,?,?)';
+$stmt = $PDO->prepare($sql);
+$stmt->bindValue(1, $name);
+$stmt->bindValue(2, $descricao);
+$stmt->bindValue(3, $preco);
 
-    var_dump($stmt->execute());
+if ($stmt->execute() === false) {
+    header('Location: /index.php?sucesso=0');
+} else {
+    header('Location: /index.php?sucesso=1');
+}
