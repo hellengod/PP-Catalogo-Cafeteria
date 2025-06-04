9fc643d933c0ab560b38020f39b2021508716298
Hellen Bianchini <hellenbiangod@gmail.com>
2025-06-04 12:58:12 -0300

    merge back to main

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hellenbianchini/sua-cafeteria-cat',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'af8b22341724d7d7bab26fc65f44c606127ea0ba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hellenbianchini/sua-cafeteria-cat' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'af8b22341724d7d7bab26fc65f44c606127ea0ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
