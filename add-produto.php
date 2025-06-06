c19d8786fb83bc6845e6a67ca11006c75598a8fb
Hellen Bianchini <hellenbiangod@gmail.com>
2025-06-06 12:37:16 -0300

    refactor: Remove código HTML repetido

--- add-produto.php
+++ add-produto.php
@@ -22,59 +22,43 @@ if ($id !== null && $id !== false) {
     $produto = $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-
+require_once 'inicio-html.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
+</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Café</title>
-    <link rel="stylesheet" href="css\styles.css">
-</head>
+<a class="button-login" href="/">Voltar</a>
+</header>
+<section class="form-section">
+    <div class="form">
+        <form class="input-form" method="post">
+            <div class="input-container">
+                <h2 class="titulo-form">Adicionar Produto</h2>
 
-<body>
-    <main>
-        <header>
-            <div class="header-container">
-                <h1 id="titulo-header">Café Catalogo</h1>
+                <div class="input-name">
+                    <label class="label-form-nome" for="name">Nome</label>
+                    <input value="<?= $produto['nome'] ?>" name="name" class="input-form-nome" type="text">
+                </div>
+                <div class="input-desc">
+                    <label class="label-form-desc" for="image">Descricao</label>
+                    <input value="<?= $produto['Descricao'] ?>" class="input-form-desc" name="descricao" type="text">
+                </div>
+                <div class="input-preco">
+                    <label class="label-form-preco" for="preco">Preco</label>
+                    <input value="<?= $produto['Preco'] ?>" name="preco" class="input-form-preco" type="text">
+                </div>
+                <div class="input-img">
+                    <label class="label-form-img" for="image">Imagem:</label>
+                    <input type="file">
+                </div>
+                <div class="input-envia">
+                    <input class="input-form-envia" type="submit">
+                </div>
+
             </div>
-            <a class="button-login" href="/">Voltar</a>
-        </header>
+        </form>
+    </div>
+</section>
 
-        <section class="form-section">
-            <div class="form">
-                <form class="input-form" method="post">
-                    <div class="input-container">
-                        <h2 class="titulo-form">Adicionar Produto</h2>
-
-                        <div class="input-name">
-                            <label class="label-form-nome" for="name">Nome</label>
-                            <input value="<?= $produto['nome'] ?>" name="name" class="input-form-nome" type="text">
-                        </div>
-                        <div class="input-desc">
-                            <label class="label-form-desc" for="image">Descricao</label>
-                            <input value="<?= $produto['Descricao'] ?>" class="input-form-desc" name="descricao"
-                                type="text">
-                        </div>
-                        <div class="input-preco">
-                            <label class="label-form-preco" for="preco">Preco</label>
-                            <input value="<?= $produto['Preco'] ?>" name="preco" class="input-form-preco" type="text">
-                        </div>
-                        <div class="input-img">
-                            <label class="label-form-img" for="image">Imagem:</label>
-                            <input type="file">
-                        </div>
-                        <div class="input-envia">
-                            <input class="input-form-envia" type="submit">
-                        </div>
-
-                    </div>
-                </form>
-            </div>
-        </section>
-
-</body>
-
-</html>
+<?php
+require_once 'fim-html.php';
+?>
